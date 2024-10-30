54743bb5f6e2a8d56e8f60efbe36b3f77ff6aad5
surajdudhatra02 <surajdudhatra0202@gmail.com>
2024-10-30 23:43:37 +0530

    build version

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const Questions = require("./db/modelQues.js")
 const allQues = require("./db/insertToDB.js")
 const connectToMongoDB = require("./db/connectDB.js");
+const path = require("path");
 
 const questionRouter = require("./routes/questionRoute.js")
 const port = process.env.PORT || 3000
@@ -11,10 +12,15 @@ const app = express()
 
 dotenv.config()
 
+const _dirname = path.resolve()
 
 app.use(express.json())
-
 app.use("/api", questionRouter)
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get("*", (req, res) => {
+    res.send(path.resolve(_dirname, "frontend", "dist", "index.html"))
+})
 
 connectToMongoDB();
 
